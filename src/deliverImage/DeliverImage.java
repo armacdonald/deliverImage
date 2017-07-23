f87f16d05d6b4140974ca33629307a585b16e2f9
Andy MacDonald <armacdonald@virginmedia.com>
2017-07-23 15:28:53 +0100

    Initial commit of Java project template.

---
+++ src/deliverImage/DeliverImage.java
@@ -0,0 +1,9 @@
+package deliverImage;
+
+import static java.lang.System.out;
+
+public class DeliverImage {
+	public static void main(String[] args) {
+		out.println("DeliverImage");
+	}
+}
---
+++ tst/deliverImage/DeliverImageTest.java
@@ -0,0 +1,12 @@
+package deliverImage;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import deliverImage.DeliverImage;
+
+public class DeliverImageTest {
+	@Test
+	public void testNothing() {
+		DeliverImage deliverImage = new DeliverImage();
+	}
+}
